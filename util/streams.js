2266b96c146bcc514394a1711d424e7b91edceda
Siarhei Balonikau <Siarhei_Balonikau@epam.com>
2018-05-24 21:12:38 +0300

    'Streams'

--- util/streams.js
+++ util/streams.js
@@ -1,18 +1,98 @@
-var program = require('commander');
+var minimist = require('minimist');
+var fs = require('fs');
+var util = require('util');
+var csv = require("csvtojson");
 
-var actions = {
-    reverse: function (str) {
-        console.log('reverse');
-    },
+function reverse() {
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function(data) {
+        process.stdout.write(reverseString(data));
+    });
+}
 
-    transform: function (str) {
-        console.log('transform');
+function transform() {
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function(data) {
+        process.stdout.write(data.toUpperCase());
+    });
+}
+
+function outputFile(file) {
+    var reader = fs.createReadStream(file);
+    reader.setEncoding('utf8');
+    reader.on('data', function(data) {
+        process.stdout.write(data);
+    });
+}
+
+function convertedFromFile(file) {
+    if (fs.existsSync(file)) {
+        csv()
+            .fromFile(file)
+            .then(function(jsonObj) {
+                process.stdout.write(JSON.stringify(jsonObj));
+            })
+    } else {
+        console.log("File doesn't exist");
     }
 }
 
-program
-  .version('0.1.0')
-  .option('-a, --action <name>', 'Action', function (name) {
-      actions[name]();
-  })
-  .parse(process.argv);
+function convertedToFile(file) {
+    if (fs.existsSync(file)) {
+        var writeStream = fs.createWriteStream(file.replace('csv', 'json'));
+        csv()
+            .fromFile(file)
+            .then(function(jsonObj) {
+                writeStream.write(JSON.stringify(jsonObj));
+            })
+    } else {
+        console.log("File doesn't exist");
+    }
+}
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+function handleCommandLine(argv) {
+    if (argv.help || argv.h || Object.keys(argv).length < 2) {
+        return printHelpMessage();
+    }
+
+    const action = argv.action || argv.a;
+    const file = argv.file || argv.f;
+
+    try {
+        switch (action) {
+            case 'reverse':
+                reverse();
+                break;
+            case 'transform':
+                transform();
+                break;
+            case 'outputFile':
+                outputFile(file);
+                break;
+            case 'convertedFromFile':
+                convertedFromFile(file);
+                break;
+            case 'convertedToFile':
+                convertedToFile(file);
+                break;
+            default:
+                console.log('You write wrong parameters');
+                printHelpMessage();
+                break;
+        }
+    } catch (e) {
+        return printHelpMessage(e);
+    }
+}
+
+function printHelpMessage(e) {
+    console.log(e);
+    console.log('Use --action or --a for choosing function');
+    console.log('Use --file or --f for setting file');
+}
+
+handleCommandLine(minimist(process.argv.slice(2)));
